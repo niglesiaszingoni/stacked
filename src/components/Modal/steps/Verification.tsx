30714696663872e46486a4833b0e7dd38d59ec8b
Nicolas Iglesias <niglesiaszingoni@gmail.com>
2025-05-19 16:58:49 +0200

    fix: restrict verification code to one number per input

--- src/components/Modal/steps/Verification.tsx
+++ src/components/Modal/steps/Verification.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { KeyboardEvent, useRef } from "react";
 
 interface Props {
   onVerificationSuccess: () => void;
@@ -9,11 +9,21 @@ export const Verification = ({ onVerificationSuccess }: Props) => {
 
   const handleInputChange = (index: number, value: string) => {
     authCode.current[index] = Number(value);
-
     if (authCode.current.length === 6) {
       onVerificationSuccess();
     }
   };
+
+  const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+    const key = event.key;
+    const value = event.currentTarget?.value;
+    const new_value = Number(value + key);
+
+    if (new_value > 9) {
+      event.preventDefault();
+    }
+  };
+
   return (
     <div className="flex flex-col gap-2">
       <span className="text-lg text-primary-grey">
@@ -25,6 +35,7 @@ export const Verification = ({ onVerificationSuccess }: Props) => {
             className="w-[62px] text-primary-light flex text-center h-20 bg-white/10 p-2.5 rounded-sm text-4xl [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
             key={index}
             type="number"
+            onKeyDown={(e) => handleKeyDown(e)}
             onChange={(e) => handleInputChange(index, e.target.value)}
           />
         ))}
